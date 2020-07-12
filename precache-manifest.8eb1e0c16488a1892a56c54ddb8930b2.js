8572cd0b41b4c0109df43f15f18861f0249fbb74
Adejumo Adeniyi <niyiade97@gmail.com>
2020-07-12 15:14:42 +0100

    Updates

---
+++ precache-manifest.8eb1e0c16488a1892a56c54ddb8930b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179fa26b11ede0d59dc82a7bcc8c1581",
+    "url": "/Dictionary-React-App/index.html"
+  },
+  {
+    "revision": "6ef8120a44b742151911",
+    "url": "/Dictionary-React-App/static/css/main.7f50b365.chunk.css"
+  },
+  {
+    "revision": "3ba4b592ec0ce4767110",
+    "url": "/Dictionary-React-App/static/js/2.0058ab78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dictionary-React-App/static/js/2.0058ab78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ef8120a44b742151911",
+    "url": "/Dictionary-React-App/static/js/main.ba587402.chunk.js"
+  },
+  {
+    "revision": "f09c8642d28330db3aab",
+    "url": "/Dictionary-React-App/static/js/runtime-main.44d29b0d.js"
+  }
+]);
